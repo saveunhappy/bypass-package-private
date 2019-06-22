8ac1858d96b290165ea4acfe0175e97c24550ab1
Bo Zhang <bo@gradle.com>
2019-06-22 19:57:13 +0800

    Sync

--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import com.github.blindpirate.extensions.CaptureSystemOutputExtension;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(createCaptureSystemOutputExtension.getClass().getName());

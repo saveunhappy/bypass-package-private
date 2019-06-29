b61ed4c9f4a958263dfa2b03fed8a4f8e109c00a
Moonstone <justwe77@163.com>
2019-06-29 22:06:12 +0800

    modify (#14)
    modify (#14)

---
+++ src/main/java/com/github/blindpirate/extensions/MyClass.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class MyClass {
+    public static Object newCaptureSystemOutputExtension(){
+        return new CaptureSystemOutputExtension();
+    }
+
+}

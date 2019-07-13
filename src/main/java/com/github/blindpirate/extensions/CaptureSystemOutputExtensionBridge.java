7e194289e341f14c4d9e1e0771482d1cd5cd181a
Richardhtw <huangtianwei_it@163.com>
2019-07-13 18:48:18 +0800

    绕过包级私有的限制 (#28)
    绕过包级私有的限制 (#28)

---
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtensionBridge.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class CaptureSystemOutputExtensionBridge {
+    public Object newInstance() {
+        return new CaptureSystemOutputExtension();
+    }
+}

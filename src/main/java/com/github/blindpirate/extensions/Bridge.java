cc5485bba1826db61038dc98b6f99b126ec54201
OrangeC <38210165+903875816@users.noreply.github.com>
2019-09-25 00:30:53 +0800

    绕过包级私有的限制 (#73)
    绕过包级私有的限制 (#73)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class Bridge {
+    public static CaptureSystemOutputExtension example(){
+        CaptureSystemOutputExtension s= new CaptureSystemOutputExtension();
+        return s;
+    }
+}

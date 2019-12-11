df43162847d85d104668164de49b7ca3da45dade
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2019-12-11 17:16:29 +0800

    Java面向对象初步练习：绕过包级私有的限制 (#104)
    Java面向对象初步练习：绕过包级私有的限制 (#104)

---
+++ src/main/java/com/github/blindpirate/extensions/extensionsHandler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.github.blindpirate.extensions;
+
+/**
+ * @author sunp
+ *
+ */
+public class extensionsHandler {
+
+	/**
+	 * 
+	 */
+	public static CaptureSystemOutputExtension createExtensionsHandler() {
+		 return new CaptureSystemOutputExtension();
+		// TODO Auto-generated constructor stub
+	}
+
+}

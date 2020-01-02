849ba2ba3fdaf9951b947215586822d457497ad1
zaker-666 <1075151259china@gmail.com>
2020-01-02 12:58:23 +0800

    实现了穿透 (#119)
    实现了穿透 (#119)

---
+++ src/main/java/com/github/blindpirate/extensions/Crossover.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Crossover {
+    public CaptureSystemOutputExtension getClas(){
+        return new CaptureSystemOutputExtension();
+    }
+}

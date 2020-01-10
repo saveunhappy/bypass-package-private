0852f6bbf7aaf5fa6df4ebc70a735f69b22acfc5
zml59 <59412107+zml59@users.noreply.github.com>
2020-01-10 23:27:48 +0800

    绕过包级封装访问私有类。 (#128)
    绕过包级封装访问私有类。 (#128)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class Bridge {
+    public static CaptureSystemOutputExtension newInstance() {
+        return new CaptureSystemOutputExtension();
+    }
+
+}

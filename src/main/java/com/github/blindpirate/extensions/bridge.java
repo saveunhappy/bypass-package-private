da5bb9159b5117530d8ccd1b6eacdf3a1cbc0e7d
nickless <32971934+bridgeren@users.noreply.github.com>
2020-02-14 21:46:11 +0800

    finish (#140)
    finish (#140)

---
+++ src/main/java/com/github/blindpirate/extensions/bridge.java
@@ -0,0 +1,13 @@
+package com.github.blindpirate.extensions;
+
+/**
+ * creat by nickless
+ *
+ * @Date 2020/2/14 21:37
+ */
+public class bridge {
+    public Object creatCaptureSystemOutputExtension(){
+        return  new CaptureSystemOutputExtension();
+    }
+
+}

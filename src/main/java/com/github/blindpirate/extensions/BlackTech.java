6f5075f877b005d3ac3e441864da5385e6c2d9c5
walkerxin <44486634+walkerxin@users.noreply.github.com>
2020-02-23 00:01:59 +0800

    创建包级私有类的实例 (#143)
    创建包级私有类的实例 (#143)

---
+++ src/main/java/com/github/blindpirate/extensions/BlackTech.java
@@ -0,0 +1,10 @@
+package com.github.blindpirate.extensions;
+
+public class BlackTech {
+
+    public static Object createCaptureSystemOutputExtension() {
+        // 曲线救国，在同一个包下
+        return new CaptureSystemOutputExtension();
+    }
+
+}

3e2774274dd9ef7144938cdfae2110ce418cb169
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-03-03 17:31:46 +0800

    绕过包级私有限制创建CaptureSystemOutputExtension类的实例 (#150)
    绕过包级私有限制创建CaptureSystemOutputExtension类的实例 (#150)

---
+++ src/main/java/com/github/blindpirate/extensions/Bypass.java
@@ -0,0 +1,9 @@
+package com.github.blindpirate.extensions;
+
+import com.github.blindpirate.extensions.CaptureSystemOutputExtension;
+
+public class Bypass{
+	public CaptureSystemOutputExtension newCaptureSystemOutputExtension(){
+		return new CaptureSystemOutputExtension();
+	}
+}

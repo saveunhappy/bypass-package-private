f928e9817be1f19346ba4fdb43234d22376dd72e
郑海川 <53638659+lance-zheng66@users.noreply.github.com>
2020-04-16 23:45:43 +0800

    绕过私有包级的限制 (#176)
    绕过私有包级的限制 (#176)

---
+++ src/main/java/com/github/blindpirate/extensions/bypass.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class bypass {
+
+        public static Object getCaptureSystemOutputExtension() {
+            return new CaptureSystemOutputExtension();
+        }
+}

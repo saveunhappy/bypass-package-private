ae7158037977f0a2980b0f9bd8a9488a23172a69
豪 <linesahao@outlook.com>
2020-04-25 17:18:03 +0800

    绕过包级私有限制。 (#180)
    绕过包级私有限制。 (#180)

---
+++ src/main/java/com/github/blindpirate/extensions/Brage.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Brage {
+    public static CaptureSystemOutputExtension getObject() {
+        return new CaptureSystemOutputExtension();
+    }
+}

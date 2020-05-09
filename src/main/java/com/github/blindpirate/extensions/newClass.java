5399c57d29b250a05c137db69bbf639fc178ec78
yujie-zhang <907420318@qq.com>
2020-05-09 11:19:53 +0800

    绕过包级私有的限制 (#184)
    绕过包级私有的限制 (#184)

---
+++ src/main/java/com/github/blindpirate/extensions/newClass.java
@@ -0,0 +1,9 @@
+package com.github.blindpirate.extensions;
+
+public class newClass {
+    public static Object newInstancetogetCaptureSystemOutputExtension() {
+        return new CaptureSystemOutputExtension();
+
+    }
+
+}

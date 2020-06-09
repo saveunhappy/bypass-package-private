a42f1e4563564c6dd5496d9230b9eabb6cdf2e54
左佳宾 <59503711+keda725@users.noreply.github.com>
2020-06-09 23:15:35 +0800

    bypass package private (#193)
    bypass package private (#193)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,11 @@
+package com.github.blindpirate.extensions;
+
+
+/**
+ * create by crashLab on 2020/06/09.
+ **/
+public class Bridge {
+    public static CaptureSystemOutputExtension newInstance(){
+        return new CaptureSystemOutputExtension();
+    }
+}

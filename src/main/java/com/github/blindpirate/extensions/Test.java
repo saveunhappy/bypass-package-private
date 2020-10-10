8054061619c4972b876e06099015ea2b30dc1298
catmmao <catmmao@gmail.com>
2020-10-10 16:30:08 +0800

    访问包级私有类 (#216)
    访问包级私有类 (#216)

---
+++ src/main/java/com/github/blindpirate/extensions/Test.java
@@ -0,0 +1,9 @@
+package com.github.blindpirate.extensions;
+
+import com.github.blindpirate.extensions.CaptureSystemOutputExtension;
+
+public class Test {
+    public static Object create() {
+        return new CaptureSystemOutputExtension();
+    }
+}

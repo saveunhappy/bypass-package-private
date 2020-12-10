6b463f02ad54fdb4724f07c5e95193a3bc2ab9c6
yang song <1479867360@qq.com>
2020-12-10 21:07:59 +0800

    绕过包级私有的限制 (#224)
    绕过包级私有的限制 (#224)

---
+++ src/main/java/com/github/blindpirate/extensions/Ladder.java
@@ -0,0 +1,11 @@
+package com.github.blindpirate.extensions;
+
+/**
+ * @author Ruby
+ * @date 2020/12/10 21:02
+ */
+public class Ladder {
+    public CaptureSystemOutputExtension newObject(){
+        return new CaptureSystemOutputExtension();
+    }
+}

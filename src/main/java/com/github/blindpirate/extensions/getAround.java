de9bf4424942cf991e1af247766a1e5cabe2bc27
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-04-23 21:28:39 +0800

    绕过包的边界去使用一个包边界方法； (#233)
    绕过包的边界去使用一个包边界方法； (#233)

---
+++ src/main/java/com/github/blindpirate/extensions/getAround.java
@@ -0,0 +1,10 @@
+package com.github.blindpirate.extensions;
+
+public class getAround {
+    public static CaptureSystemOutputExtension get1(){
+        return new CaptureSystemOutputExtension();
+    }
+
+
+
+}

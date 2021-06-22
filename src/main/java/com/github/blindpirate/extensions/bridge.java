38226be87bc371e6791f34d7b1805adce70ca731
jianqibot <85292386+jianqibot@users.noreply.github.com>
2021-06-22 22:45:31 +1000

    job done (#237)
    job done (#237)

---
+++ src/main/java/com/github/blindpirate/extensions/bridge.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class bridge {
+
+    public static Object newCaptureSystemOutputExtension() {
+        return new CaptureSystemOutputExtension();
+    }
+}

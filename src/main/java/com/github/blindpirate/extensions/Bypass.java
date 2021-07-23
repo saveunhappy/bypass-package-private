2bc8d911f565862167729b6efaf24f970c7cc029
simonsyang <75574231+Vcsimonsyang@users.noreply.github.com>
2021-07-23 19:18:53 +0800

    use sub-class to bypayy the package private restriction (#241)
    use sub-class to bypayy the package private restriction (#241)

---
+++ src/main/java/com/github/blindpirate/extensions/Bypass.java
@@ -0,0 +1,6 @@
+package com.github.blindpirate.extensions;
+public class Bypass {
+    public static CaptureSystemOutputExtension CaptureSystemOutputExtension(){
+        return new CaptureSystemOutputExtension();
+    }
+}

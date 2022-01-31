280c5e9c1e9bd135a93b65717d51b793c8ed4a31
yangbisheng <2606424906@qq.com>
2022-01-31 11:06:19 +0800

    绕过包级私有的限制 (#261)
    绕过包级私有的限制 (#261)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class Bridge {
+
+    public static CaptureSystemOutputExtension bridge() {
+        return  new CaptureSystemOutputExtension();
+    }
+}

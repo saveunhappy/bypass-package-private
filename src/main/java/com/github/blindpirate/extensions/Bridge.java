343a9a0b87f852de48217eac09d36086f58debac
yangbisheng <2606424906@qq.com>
2022-08-04 00:18:22 +0800

    bypass package private (#282)
    bypass package private (#282)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,8 @@
+package com.github.blindpirate.extensions;
+
+public class Bridge {
+
+    public static CaptureSystemOutputExtension createCaptureSystemOutputExtension() {
+        return new CaptureSystemOutputExtension();
+    }
+}

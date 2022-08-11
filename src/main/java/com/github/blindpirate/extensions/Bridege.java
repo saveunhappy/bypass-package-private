7b8053df85dbf62eb90d4543bcb44d5f2e370d97
Chen <99600709+Supertramp-Chen@users.noreply.github.com>
2022-08-11 15:37:52 +0800

    yyyyyyyyyyyy (#283)
    yyyyyyyyyyyy (#283)

---
+++ src/main/java/com/github/blindpirate/extensions/Bridege.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Bridege {
+    public static CaptureSystemOutputExtension getClassofCaptureSystemOutputExtension(){
+        return new CaptureSystemOutputExtension();
+    }
+}
